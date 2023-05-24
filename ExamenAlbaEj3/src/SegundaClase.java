cae53cfaf0fbc712633975b5ebc6cd36666e5b09
Alba <albasmigallon@gmail.com>
2023-05-24 13:53:24 +0200

    Hola

--- ExamenAlbaEj3/src/SegundaClase.java
+++ ExamenAlbaEj3/src/SegundaClase.java
@@ -1,12 +1,24 @@
 import java.util.ArrayList;
-
+/**
+ * 
+ * @author alumno Alba
+ *
+ */
 public class SegundaClase {
+	/**
+	 * arraylist que almacena edades
+	 */
 	ArrayList<Integer> edades =new ArrayList<Integer>();
 	
-	
+	/**
+	 * 
+	 * @param edades constructor
+	 */
 		public SegundaClase(ArrayList<Integer> edades) {
 			
 			this.edades = edades;
+			
+			
 		}
 
 }
